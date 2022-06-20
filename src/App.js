0850cce0fff930515612999f9319c3126bc5c3dd
jahidshakil <jahidaladin@gmail.com>
2022-06-20 22:28:05 +0600

    project setup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Hello World
+    <div className="flex w-screen h-screen items-center justify-center">
+      <p className='text-2xl text-blue-500'>Hello World</p>
     </div>
   );
 }
